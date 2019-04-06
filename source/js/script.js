9d1ed025901ad574780d4e2662772dae7efcd251
Max Manchak <manchak.max@gmail.com>
2019-04-06 13:28:38 +0300

    bugfix

--- source/js/script.js
+++ source/js/script.js
@@ -9,13 +9,15 @@
 
 'use strict';
 
-jQuery(function($) {
+jQuery( function ( $ ) {
+	// активация кастомного скроллбара
 	if ($(window).width() > 640) {
 		$('.scrollbar').mCustomScrollbar({
 			scrollInertia: 150,
 		});
 	}
 
+	// взаимодействие с формой
 	$('.form-group input')
 		.focus(function() {
 			$(this).closest('.form-group').addClass('active');
@@ -24,21 +26,24 @@ jQuery(function($) {
 			if ($(this).val() === '') {
 				$(this).closest('.form-group').removeClass('active');
 			}
-		});
+		} );
+
+
+// открытие popup c новостями
 	var popupOpen = () => {
-		$('.articles-list').click(function(e) {
-			e.preventDefault();
+		if ($(window).width() > 640 && $(window).width() < 1900) {
 			$('.popup-newspaper').addClass('open');
-		});
+		}
 	};
-	if ($(window).width() > 640 && $(window).width() < 1900) {
-		popupOpen();
-	}
+	// закрытие popup
+	$('.popup-newspaper__close').click(function(e) {
+		e.preventDefault();
+		$('.popup-newspaper').removeClass('open');
+	});
+
+
 
 	window.addEventListener('resize',	function() {
-			if ($(window).width() > 640 && $(window).width() < 1900) {
-				popupOpen();
-			}
 			if ($(window).width() < 640) {
 				scrollImageLeft();
 			}
@@ -46,23 +51,25 @@ jQuery(function($) {
 		false
 	);
 
-	$('.popup-newspaper__close').click(function(e) {
-		e.preventDefault();
-		$('.popup-newspaper').removeClass('open');
-	});
 
+
+
+
+// открытие меню на мобильном
 	$('.navigation__open').on('click', function(e) {
 		e.preventDefault();
 		$('.navigation').addClass('open');
 		$('body').addClass('fixed');
 	});
-
+// закрытие меню на мобильном
 	$('.navigation__close').on('click', function(e) {
 		e.preventDefault();
 		$('.navigation').removeClass('open');
 		$('body').removeClass('fixed');
 	});
 
+
+// cмена новостей по клику без перезагрузки
 	function showContent(link) {
 		$.ajax({
 			url: link,
@@ -78,14 +85,20 @@ jQuery(function($) {
 		});
 	}
 
-	$('.articles-list a').click(function(e) {
-		e.preventDefault();
+	if ( $( window ).width() < 1024 && $( window ).width() > 640 ) {
+		$('.articles-list a').click(function(e) {
+			e.preventDefault();
+			var link = $(this).attr('href');
+			$( '#flipbook' ).turn( 'destroy' );
+			popupOpen();
+			changeSizeBook();
 
-		var link = $(this).attr('href');
-		$('#flipbook').turn('destroy');
-		showContent(link);
-	});
+			showContent(link);
+		});
+	}
 
+
+	// отображение логотипов  при прокрутке
 	if ($('*').is('.article__img-logo')) {
 		$('.main').scroll(function() {
 			$('.article__img-logo').addClass('active');
@@ -95,6 +108,8 @@ jQuery(function($) {
 		});
 	}
 
+
+	// перелистывание страниц новостей
 	var flipbook = () => {
 		$('#flipbook').turn({
 			when: {
@@ -110,29 +125,34 @@ jQuery(function($) {
 		flipPages(pages);
 	};
 
+
+	// кнопки переключения новостей
 	$('.popup-newspaper__next').click(function() {
 		$('#flipbook').turn('next');
 	});
 	$('.popup-newspaper__prev').click(function() {
 		$('#flipbook').turn('previous');
-	});
+	} );
+
 	if ($('*').is('#flipbook')) {
 		flipbook();
 	}
+
 
 	if ($(window).width() < 640) {
 		$('.scrollbar').wrap('<div class="scrollbar-wrap"></div>');
 	}
 
-	function scrollImageLeft() {
+	// центрирование по середине на второй странице
+	var scrollImageLeft = () => {
 		var width = $( window ).width();
 		var imgWidth = $( '.article-2__wrap:not(:first-child) img' ).width();
 		$( '.article-2__wrap' ).scrollLeft( imgWidth / 2 - width / 2 );
-
 	}
+	scrollImageLeft();
 
+//  отображение стрелок при крокрутке на второй странице
 	$( window ).scroll( function () {
-
 		var offsetMain = $( '.left-block--2' ).offset().top;
 		if ( (offsetMain  > ($( window ).height() - 200) ) && ($( window ).width() < 640 )) {
 			$('.article--2').addClass( 'scroll' );
@@ -141,7 +161,15 @@ jQuery(function($) {
 		}
 	} );
 
-	scrollImageLeft();
+	var changeSizeBook = () => {
+		var windowWidth = $( window ).width();
+		var windowHeight = $( window ).height();
+		$( '#flipbook' ).css( {
+			'width': (windowWidth - 80) +'px',
+			'height': (windowHeight - 130) +'px',
+		})
+	}
+
 });
 
 
